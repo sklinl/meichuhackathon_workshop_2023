c6110096c4d720226d10b635df9be30cfcc92c11
sklinl <kevinlinsk19@gmail.com>
2023-10-14 18:59:20 +0800

    to teach mode

--- backend/app/routes/xray_detection.py
+++ backend/app/routes/xray_detection.py
@@ -10,16 +10,5 @@ _logger = logging.getLogger("config")
 
 @bp.route('/upload', methods=['POST'])
 def upload_image():
-    if 'image' in request.files:
-        image = request.files['image']
-
-        # Process the uploaded image here, for example, save it to a folder
-        # image.save('path/to/save/image.jpg')
-        if ("abnormal" in image.filename):
-            custom_metrics.abnormal_counter.inc()
-
-        result = f'{image.filename} is abnormal. Please check it and notice to relevant personnel.'
-        _logger.info(result)
-
-        return Response(result, 200)
-    return Response('No image provided', 400)
+    # TODO
+    return Response("test", 200)
--- frontend/src/pages/AnalysisResult.js
+++ frontend/src/pages/AnalysisResult.js
@@ -6,92 +6,8 @@ import Layouts from '../components/Layouts';
 import TypingEffect from '../utils/typingEffect';
 
 
-const getBase64 = (file) =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-  });
 
 function AnalysisResult() {
-
-  const [fileList, setFileList] = useState([]);
-  const [previewOpen, setPreviewOpen] = useState(false);
-  const [previewImage, setPreviewImage] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [showResult, setShowResult] = useState(false);
-  const [responseText, setResponseText] = useState('');
-  const [isTypingComplete, setIsTypingComplete] = useState(false);
-
-  const handleTypingComplete = () => {
-    setIsTypingComplete(true);
-  };
-
-
-  const handleUpload = () => {
-    const backendServiceUrl = process.env.REACT_APP_BACKEND_SERVICE_URL;
-    setLoading(true);
-
-    if (fileList.length === 0) {
-      message.warning('Please select an image to upload.');
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append('image', fileList[0].originFileObj);
-
-    fetch(`${backendServiceUrl}/upload`, {
-      method: 'POST',
-      body: formData,
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          setLoading(false);
-          setShowResult(true)
-          message.success('Image uploaded successfully.');
-        } else {
-          message.error('Failed to upload image.');
-        }
-        return response.text()
-      }).then((text)=>{
-        console.log(text);
-        setResponseText(text);
-      }).catch((error) => {
-        message.error('An error occurred while uploading the image.');
-        console.log(error);
-      });
-
-  };
-
-  const handleCancel = () => {
-    setPreviewOpen(false);
-  };
-
-  const handleRemove = () => {
-    setResponseText('');
-  };
-
-
-  const handlePreview = async file => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
-    }
-    setPreviewImage(file.url || file.preview);
-    setPreviewOpen(true);
-  };
-
-  const handleChange = (info) => {
-    setFileList(info.fileList);
-  };
-
-
-  const uploadButton = (
-    <div >
-      <UploadOutlined />
-      <div className="ant-upload-text" >Upload</div>
-    </div>
-  );
 
 
   return (
@@ -102,54 +18,11 @@ function AnalysisResult() {
           <Col span={24}>
             <div className="space-align-block">
 
-              <Upload
-                listType="picture-card"
-                fileList={fileList}
-                onChange={handleChange}
-                onPreview={handlePreview}
-                onRemove={handleRemove}
-                customRequest={({ onSuccess, onError, file }) => {
-                  // Handle file upload logic here
-                  // You can send the file to your server or process it as needed
-                  // Example: send the file to a server using fetch
-                  onSuccess();
-                }}
-              >
-                {fileList.length < 1 && (
-                  uploadButton
-                )}
-              </Upload>
-              <Modal open={previewOpen} footer={null} onCancel={handleCancel}>
-                <img style={{ width: '100%' }} src={previewImage} />
-              </Modal>
+              <p> coding...</p>
 
-              <Button
-                type="primary"
-                onClick={handleUpload}
-                loading={loading}
-                disabled={fileList.length < 1}
-              >
-                分析
-              </Button>
-
-              {showResult &&
-                (<div>
-
-                  <TypingEffect
-                    text={responseText}
-                    speed={100}
-                    onTypingComplete={handleTypingComplete}
-                  />
-                </div>)
-              }
             </div>
           </Col>
         </Row>
-
-
-        <Spin spinning={loading}>
-          { !loading }
-        </Spin>
 
       </div>
     </Layouts>
